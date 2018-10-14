c43ee2d74637f7d542b8266ad0509248c83164c8
Larry Garfield <larry@garfieldtech.com>
2018-10-15 00:56:48 +0200

    Prepare structure for supporting recording the transaction log. It's not working yet.

--- src/Collection.php
+++ src/Collection.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Crell\Historia;
 
-use Crell\Historia\Shelf\ShelfInterface;
-
 class Collection
 {
 
@@ -37,8 +35,7 @@ class Collection
 
     public function initializeSchema() : self
     {
-
-        $res = $this->conn->query("CREATE TABLE IF NOT EXISTS " . $this->tableName('documents') . "(
+        $this->conn->query("CREATE TABLE IF NOT EXISTS " . $this->tableName('documents') . "(
             uuid VARCHAR(36),
             language VARCHAR(5),
             updated TIMESTAMP DEFAULT NOW(),
@@ -49,11 +46,19 @@ class Collection
             PRIMARY KEY (uuid, language)
         ) WITH SYSTEM VERSIONING");
 
-        $res = $this->conn->query('CREATE FUNCTION CURRENT_XID() RETURNS VARCHAR(18)
+        $this->conn->query('CREATE FUNCTION CURRENT_XID() RETURNS VARCHAR(18)
             BEGIN
                 RETURN (SELECT TRX_ID FROM INFORMATION_SCHEMA.INNODB_TRX
                         WHERE TRX_MYSQL_THREAD_ID = CONNECTION_ID());
             END');
+
+        $this->conn->query("CREATE TABLE IF NOT EXISTS " . $this->tableName('transactions') . "(
+            transaction VARCHAR(36),
+            recorded_at TIMESTAMP DEFAULT NOW(),
+            affected TEXT,
+            CHECK (JSON_VALID(affected)),
+            PRIMARY KEY (transaction)
+        )");
 
         return $this;
     }
@@ -141,10 +146,44 @@ class Collection
     public function commit(Commit $commit)
     {
         $this->withTransaction(function (\PDO $conn) use ($commit) {
+            $affected = [];
+
+            $tableName = $this->tableName('documents');
+
             $addRecords = $commit->getAddRecords();
             if (count($addRecords)) {
                 foreach ($addRecords as $record) {
-                    $query = sprintf("INSERT INTO %s SET document=:document, language=:language, uuid=:uuid ON DUPLICATE KEY 
+                    // Doing it the ugly multi-query way may not be necessary; it was only done to try and figure out
+                    // why the transaction ID is only sometimes readable.  The ODKU is probably workable.
+                    // Of course, in a just world we'd use an ANSI MERGE query, but the world is not just.
+
+                    $query = sprintf('SELECT 1 FROM %s WHERE uuid=:uuid AND language=:language', $tableName);
+                    $values = [
+                        ':uuid' => $record->uuid,
+                        ':language' => $record->language,
+                    ];
+                    $stmt = $conn->prepare($query);
+                    $stmt->execute($values);
+
+                    if (!$stmt->fetchColumn()) {
+                        $stmt = $conn->prepare(sprintf('INSERT INTO %s (uuid, language, document) VALUES (:uuid, :language, :document)', $tableName));
+                        $stmt->execute([
+                            ':uuid' => $record->uuid,
+                            ':language' => $record->language,
+                            ':document' => $record->document,
+                        ]);
+                    }
+                    else {
+                        $stmt = $conn->prepare(sprintf('UPDATE %s SET document=:document WHERE uuid=:uuid AND language=:language', $tableName));
+                        $stmt->execute([
+                            ':uuid' => $record->uuid,
+                            ':language' => $record->language,
+                            ':document' => $record->document,
+                        ]);
+                    }
+
+                    /*
+                    $query = sprintf("INSERT INTO %s SET document=:document, language=:language, uuid=:uuid ON DUPLICATE KEY
                         UPDATE document=:document, language=:language, updated=NOW()", $this->tableName('documents'));
                     $values = [
                         ':uuid' => $record->uuid,
@@ -153,6 +192,9 @@ class Collection
                     ];
                     $stmt = $conn->prepare($query);
                     $stmt->execute($values);
+                    */
+
+                    $affected['added'][] = ['uuid' => $record->uuid, 'language' => $record->language];
                 }
             }
 
@@ -163,6 +205,43 @@ class Collection
                 $query = sprintf('DELETE FROM %s WHERE language=? AND uuid IN (%s)', $this->tableName('documents'), $placeholders);
                 $stmt = $conn->prepare($query);
                 $stmt->execute($values);
+                // @todo This is the wrong language, but we'll fix it for this whole block later.
+                foreach ($deleteIds as $uuid) {
+                    $affected['deleted'][] = ['uuid' => $uuid, 'language' => $this->language];
+                }
+            }
+
+            if (count($affected)) {
+                // This whole section is horribly buggy. The transaction is always present i nthe table when selecting the whole thing.
+                // However, reading just the current transaction's ID fails deterministically but unpredictably. I have NFI why
+                // it fails sometimes and not others, but once a test starts failing it always fails.
+                // It seems to be an issue with the trx_mysql_thread_id only sometimes matching the connection ID. This needs
+                // further research.
+
+                //$query = sprintf('INSERT INTO %s SET transaction=CURRENT_XID(), affected=:affected', $this->tableName('transactions'));
+                //$stmt = $conn->prepare($query);
+                //$stmt->execute([':affected' => json_encode($affected)]);
+
+                /*
+                This is all debugging code.
+
+                $trxs = $conn->query("SELECT * FROM INFORMATION_SCHEMA.INNODB_TRX")->fetchAll(\PDO::FETCH_ASSOC);
+                print_r($trxs);
+
+                $connid = $conn->query("SELECT CONNECTION_ID() AS conn_id")->fetchObject();
+                if ($connid) {
+                    print_r($connid);
+                } else {
+                    print_r($connid);
+                }
+
+                $xid = $conn->query("SELECT CURRENT_XID() AS xid")->fetchObject();
+                if ($xid) {
+                    print_r($xid);
+                } else {
+                    print_r($xid);
+                }
+                */
             }
         });
     }
